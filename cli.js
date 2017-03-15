6e25f87ac14d1356df3e14489417576ac0702b3d
David Cornu <davidjcornu@gmail.com>
2017-03-15 10:30:01 -0400

    Refactor lint() so it isn't using global state

--- cli.js
+++ cli.js
@@ -2,18 +2,16 @@
 'use strict';
 
 const commander = require('commander');
+const _ = require('lodash');
 
 const pkg = require('./package.json');
-const linters = require('./index').linters;
+const registeredLinters = require('./index').linters;
 const Reporter = require('./reporter');
-
-const keys = Object.keys(linters);
-const reporter = new Reporter();
 
 commander
   .version(pkg.version)
   .arguments('<path>')
-  .action(path => lint(path))
+  .action(path => lint(path, _.values(registeredLinters)))
   .usage('[options] <path>')
   .parse(process.argv);
 
@@ -21,14 +19,17 @@ if (!process.argv.slice(2).length) {
   commander.outputHelp();
 }
 
-function lint(path) {
-  const key = keys.pop();
-  if (key) {
-    const linter = new linters[key](path);
-    linter.run(reporter)
-      .then(() => {lint(path)});
+function lint(path, linters, reporter = new Reporter()) {
+  const Linter = linters[0];
+
+  if (Linter) {
+    const linter = new Linter(path);
+    linter.run(reporter).then(() => {
+      lint(path, linters.slice(1), reporter);
+    });
   } else {
     reporter.finalize();
+
     if (reporter.failures.length > 0) {
       process.on('exit', () => process.exit(1));
     }
