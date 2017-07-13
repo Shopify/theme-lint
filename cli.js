29c9281485845710f821856a3b62e9f36f2f7ecc
Miguel Palau <mpalauzarza@gmail.com>
2017-07-13 17:56:27 -0500

    deleting unused dependency on cli.js file

--- cli.js
+++ cli.js
@@ -1,32 +1,31 @@
 #!/usr/bin/env node
-'use strict';
+'use strict'
 
-const commander = require('commander');
-const _ = require('lodash');
+const commander = require('commander')
 
-const pkg = require('./package.json');
-const {runAll} = require('./index');
+const pkg = require('./package.json')
+const {runAll} = require('./index')
 
 commander
   .version(pkg.version)
   .arguments('<path>')
   .action(path => lint(path))
   .usage('[options] <path>')
-  .parse(process.argv);
+  .parse(process.argv)
 
 if (!process.argv.slice(2).length) {
-  commander.outputHelp();
+  commander.outputHelp()
 }
 
-function lint(path) {
+function lint (path) {
   return runAll(path).then(reporter => {
-    reporter.finalize();
+    reporter.finalize()
 
     if (reporter.failures.length > 0) {
-      process.on('exit', () => process.exit(1));
+      process.on('exit', () => process.exit(1))
     }
   }).catch(e => {
-    console.log(e);
-    process.on('exit', () => process.exit(1));
-  });
+    console.log(e)
+    process.on('exit', () => process.exit(1))
+  })
 }
